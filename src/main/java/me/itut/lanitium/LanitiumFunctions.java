74d2f015c4830549893932768febec2f381fba3f
iTut <maykaitut@gmail.com>
2025-03-27 14:22:52 +0300

    safe_equal(a, b)

--- src/main/java/me/itut/lanitium/LanitiumFunctions.java
+++ src/main/java/me/itut/lanitium/LanitiumFunctions.java
@@ -515,20 +515,20 @@ public class LanitiumFunctions {
         return BooleanValue.of(isSelfReferential(container, Collections.newSetFromMap(new IdentityHashMap<>())));
     }
 
-    public static boolean isSelfReferential(Value container, Set<Value> found) {
-        if (found.contains(container))
-            return true;
+    public static boolean isSelfReferential(Value container, Set<Object> found) {
         return switch (container) {
-            case ListValue list -> {
-                found.add(container);
-                if (list.getItems().stream().anyMatch(found::contains)) yield true;
-                found.remove(container);
+            case ListValue l -> {
+                if (found.contains(l.getItems())) yield true;
+                found.add(l.getItems());
+                if (l.getItems().stream().anyMatch(found::contains)) yield true;
+                found.remove(l.getItems());
                 yield false;
             }
-            case MapValue map -> {
-                found.add(container);
-                if (map.getMap().entrySet().stream().anyMatch(e -> found.contains(e.getValue()) || found.contains(e.getKey()))) yield true;
-                found.remove(container);
+            case MapValue m -> {
+                if (found.contains(m.getMap())) yield true;
+                found.add(m.getMap());
+                if (m.getMap().entrySet().stream().anyMatch(e -> found.contains(e.getValue()) || found.contains(e.getKey()))) yield true;
+                found.remove(m.getMap());
                 yield false;
             }
             default -> false;
@@ -540,24 +540,24 @@ public class LanitiumFunctions {
         return StringValue.of(safeString(container, Collections.newSetFromMap(new IdentityHashMap<>())));
     }
 
-    public static String safeString(Value container, Set<Value> traversed) {
-        if (traversed.contains(container))
-            return container instanceof MapValue ? "{...}" : "[...]";
+    public static String safeString(Value container, Set<Object> traversed) {
         return switch (container) {
             case ListValue l -> {
-                traversed.add(l);
+                if (traversed.contains(l.getItems())) yield "[...]";
+                traversed.add(l.getItems());
                 try {
                     yield "[" + l.getItems().stream().map(e -> safeString(e, traversed)).collect(Collectors.joining(", ")) + "]";
                 } finally {
-                    traversed.remove(l);
+                    traversed.remove(l.getItems());
                 }
             }
             case MapValue m -> {
-                traversed.add(m);
+                if (traversed.contains(m.getMap())) yield "{...}";
+                traversed.add(m.getMap());
                 try {
                     yield "{" + m.getMap().entrySet().stream().map(e -> safeString(e.getKey(), traversed) + ": " + safeString(e.getValue(), traversed)).collect(Collectors.joining(", ")) + "}";
                 } finally {
-                    traversed.remove(m);
+                    traversed.remove(m.getMap());
                 }
             }
             default -> container.getString();
@@ -565,30 +565,98 @@ public class LanitiumFunctions {
     }
 
     @ScarpetFunction
+    public static Value safe_equal(Value a, Value b) {
+        return BooleanValue.of(safeEqual(a, b, Collections.newSetFromMap(new IdentityHashMap<>())));
+    }
+
+    public static boolean safeEqual(Value a, Value b, Set<Object> traversed) {
+        if (a == b) return true;
+        if (a instanceof ListValue != b instanceof ListValue
+         || a instanceof MapValue != b instanceof MapValue)
+            return false;
+        return switch (a) {
+            case ListValue v -> {
+                List<Value> la = v.getItems(), lb = ((ListValue)b).getItems();
+                if (la == lb) yield true;
+                if (traversed.contains(la)) yield traversed.contains(lb);
+                if (traversed.contains(lb)) yield false;
+                if (la.size() != lb.size()) yield false;
+                if (la.isEmpty()) yield true;
+                traversed.add(la);
+                traversed.add(lb);
+                for (Iterator<Value> ita = la.iterator(), itb = lb.iterator(); ita.hasNext();) {
+                    Value ea = ita.next(), eb = itb.next();
+                    if (!safeEqual(ea, eb, traversed)) yield false;
+                }
+                traversed.remove(la);
+                traversed.remove(lb);
+                yield true;
+            }
+            case MapValue v -> {
+                Map<Value, Value> ma = v.getMap(), mb = ((MapValue)b).getMap();
+                if (ma == mb) yield true;
+                if (traversed.contains(ma)) yield traversed.contains(mb);
+                if (traversed.contains(mb)) yield false;
+                if (ma.size() != mb.size()) yield false;
+                if (ma.isEmpty()) yield true;
+                traversed.add(ma);
+                traversed.add(mb);
+                LinkedList<Map.Entry<Value, Value>> entries = new LinkedList<>(mb.entrySet().stream().toList());
+                for (Map.Entry<Value, Value> ea : ma.entrySet()) {
+                    Value ka = ea.getKey(), va = ea.getValue();
+                    @SuppressWarnings("unchecked")
+                    Map.Entry<Value, Value>[] entry = new Map.Entry[] {null};
+                    try {
+                        entries.removeIf(eb -> {
+                            if (entry[0] != null || !safeEqual(ka, eb.getKey(), traversed)) return false;
+                            if (!safeEqual(va, eb.getValue(), traversed)) throw new SafeNotEqualException();
+                            entry[0] = eb;
+                            return true;
+                        });
+                    } catch (SafeNotEqualException ignored) {
+                        yield false;
+                    }
+                    if (entry[0] == null) yield false;
+                }
+                assert entries.isEmpty();
+                traversed.remove(ma);
+                traversed.remove(mb);
+                yield true;
+            }
+            default -> a.equals(b);
+        };
+    }
+
+    private static class SafeNotEqualException extends RuntimeException {}
+
+    @ScarpetFunction
     public static Value safe_copy(Value container) {
         return safeCopy(container, new IdentityHashMap<>());
     }
 
-    private static Value safeCopy(Value container, IdentityHashMap<Value, Value> copied) {
-        if (copied.containsKey(container)) return copied.get(container);
+    private static Value safeCopy(Value container, IdentityHashMap<Object, Value> copied) {
         return switch (container) {
             case ListValue l -> {
+                if (copied.containsKey(l.getItems())) yield copied.get(l.getItems());
                 List<Value> list = new ArrayList<>(l.length());
                 ListValue copy = ListValue.wrap(list);
-                copied.put(l, copy);
+                if (l.length() == 0) yield copy;
+                copied.put(l.getItems(), copy);
                 list.addAll(l.getItems().stream().map(e -> safeCopy(e, copied)).toList());
-                copied.remove(l);
+                copied.remove(l.getItems());
                 yield copy;
             }
             case MapValue m -> {
+                if (copied.containsKey(m.getMap())) yield copied.get(m.getMap());
                 Map<Value, Value> map = HashMap.newHashMap(m.length());
                 MapValue copy = MapValue.wrap(map);
-                copied.put(m, copy);
+                if (m.length() == 0) yield copy;
+                copied.put(m.getMap(), copy);
                 map.putAll(m.getMap().entrySet().stream().collect(Collectors.toMap(
                     e -> safeCopy(e.getKey(), copied),
                     e -> safeCopy(e.getValue(), copied)
                 )));
-                copied.remove(m);
+                copied.remove(m.getMap());
                 yield copy;
             }
             default -> container.deepcopy();
@@ -644,22 +712,24 @@ public class LanitiumFunctions {
             new IdentityHashMap<>());
     }
 
-    private static Value safeDeepMap(Value container, UnaryOperator<Value> lf, BinaryOperator<Value> kf, BinaryOperator<Value> vf, BinaryOperator<Value> ref, IdentityHashMap<Value, Value> mapped) {
+    private static Value safeDeepMap(Value container, UnaryOperator<Value> lf, BinaryOperator<Value> kf, BinaryOperator<Value> vf, BinaryOperator<Value> ref, IdentityHashMap<Object, Value> mapped) {
         return switch (container) {
             case ListValue l -> {
-                if (mapped.containsKey(l)) yield ref.apply(l, mapped.get(l));
+                if (mapped.containsKey(l.getItems())) yield ref.apply(l, mapped.get(l.getItems()));
                 List<Value> list = new ArrayList<>(l.length());
                 ListValue copy = ListValue.wrap(list);
-                mapped.put(l, copy);
+                if (l.length() == 0) yield copy;
+                mapped.put(l.getItems(), copy);
                 list.addAll(l.getItems().stream().map(v -> lf.apply(safeDeepMap(v, lf, kf, vf, ref, mapped))).toList());
-                mapped.remove(l);
+                mapped.remove(l.getItems());
                 yield copy;
             }
             case MapValue m -> {
-                if (mapped.containsKey(m)) yield ref.apply(m, mapped.get(m));
+                if (mapped.containsKey(m.getMap())) yield ref.apply(m, mapped.get(m.getMap()));
                 Map<Value, Value> map = HashMap.newHashMap(m.length());
                 MapValue copy = MapValue.wrap(map);
-                mapped.put(m, copy);
+                if (m.length() == 0) yield copy;
+                mapped.put(m.getMap(), copy);
                 map.putAll(m.getMap().entrySet().stream().map(e -> new AbstractMap.SimpleEntry<>(
                     safeDeepMap(e.getKey(), lf, kf, vf, ref, mapped),
                     safeDeepMap(e.getValue(), lf, kf, vf, ref, mapped)
@@ -667,7 +737,7 @@ public class LanitiumFunctions {
                     e -> kf.apply(e.getKey(), e.getValue()),
                     e -> vf.apply(e.getKey(), e.getValue())
                 )));
-                mapped.remove(m);
+                mapped.remove(m.getMap());
                 yield copy;
             }
             default -> container;
